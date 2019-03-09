dc1f6cdd0f2ef99f826c6136ce4a8a9c50d33ce9
Alan Morrison <alan.morrison@taipannetworks.com.au>
2019-03-09 15:55:48 +1100

    initial commit.

---
+++ src/SourceLinkExample.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SourceLinkExample.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 c = new Class1();
+            c.TestMethod();
+        }
+    }
+}
