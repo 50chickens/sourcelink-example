18ba2940338a2f7e018fe964fe6313cd22afcf5e
Alan Morrison <alan.morrison@taipannetworks.com.au>
2019-04-14 19:20:16 +1000

    changed project layout

---
+++ src/Tnn.Library.SourceLinkProvider/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tnn.Library.SourceLinkProvider
+{
+    public class Class1
+    {
+        public int Method1(int i)
+        {
+            var r = i++;
+            return r;
+        }
+    }
+}
